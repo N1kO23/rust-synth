d14430fe25d1273fa7e83af0e0fe4789ad1d6d3a
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-10 23:11:26 +0200

    refactor a bit
    Co-Authored-By: Niko Huuskonen <59277828+N1kO23@users.noreply.github.com>

--- src/waves.rs
+++ src/waves.rs
@@ -21,7 +21,7 @@ pub fn sawtooth_wave(
   for i in 0..(sample_rate as u32) {
     let size = sample_rate / frequency;
     let pos = i as f32 % size;
-    samples.push(((start_phase + (pos / size)) % 1.0 - 0.5) * amplitude);
+    samples.push(amplitude * ((start_phase + (pos / size)) % 1.0 - 0.5));
   }
   return samples;
 }
@@ -31,7 +31,7 @@ pub fn sine_wave(amplitude: f32, frequency: f32, start_phase: f32, sample_rate: 
   for i in 0..(sample_rate as u32) {
     let size = sample_rate / frequency;
     let pos = i as f32 % size;
-    samples.push(((start_phase + pos / size) * (2.0 * PI) - 0.5).sin() * amplitude);
+    samples.push(amplitude * ((start_phase + pos / size) * (2.0 * PI) - 0.5).sin());
   }
   return samples;
 }
@@ -46,7 +46,7 @@ pub fn triangle_wave(
   for i in 0..(sample_rate as u32) {
     let size = sample_rate / frequency;
     let pos = i as f32 % size;
-    samples.push((((start_phase + (pos / size)) % 1.0 * 2.0 - 1.0).abs() - 0.5) * amplitude);
+    samples.push(amplitude * (((start_phase + (pos / size)) % 1.0 * 2.0 - 1.0).abs() - 0.5));
   }
   return samples;
 }
@@ -57,11 +57,11 @@ pub fn square_wave(amplitude: f32, frequency: f32, start_phase: f32, sample_rate
     let size = sample_rate / frequency;
     let pos = i as f32 % size;
     samples.push(
-      ((frequency * (start_phase + (2.0 * PI * pos) / sample_rate))
-        .sin()
-        .floor()
-        + 0.5)
-        * amplitude,
+      amplitude
+        * ((frequency * (start_phase + (2.0 * PI * pos) / sample_rate))
+          .sin()
+          .floor()
+          + 0.5),
     );
   }
   return samples;
